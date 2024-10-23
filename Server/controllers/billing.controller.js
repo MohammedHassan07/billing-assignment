19a344a31c7d93dbd854ebcbf7060b9bb636dc00
MohammedHassan07 <mhassancs260@gmail.com>
2024-10-23 21:50:57 +0530

    FEAT: edit, update, delete bill

--- Server/controllers/billing.controller.js
+++ Server/controllers/billing.controller.js
@@ -53,6 +53,86 @@ const createBill = async (req, res) => {
     }
 }
 
+// function to update bill
+const updateBill = async (req, res) => {
+        try {
+            const { id } = req.params
+            const { customerId, products } = req.body
+    
+            const customer = await customerModel.findById(customerId)
+            if (!customer) return res.status(404).json({ message: "Customer not found" })
+    
+            let totalAmount = 0
+            const billingProducts = []
+    
+            for (const item of products) {
+                const product = await productModel.findById({_id: item.productId})
+                if (!product) return res.status(404).json({ message: `Product not found with id: ${item.productId}` })
+    
+                totalAmount += product.productPrice * item.quantity
+    
+                billingProducts.push({
+                    product: item.productId,
+                    quantity: item.quantity
+                })
+            }
+    
+            // Update billing record
+            const updatedBilling = await billingModel.findByIdAndUpdate(
+                id,
+                { customer: customerId, products: billingProducts, totalAmount },
+                { new: true } // Return updated document
+            )
+    
+            if (!updatedBilling) return res.status(404).json({ message: 'Billing record not found' })
+    
+            res.status(200).json({ message: 'Billing updated successfully', billing: updatedBilling })
+    
+        } catch (error) {
+            console.log(error)
+            res.status(500).json({ message: error.message })
+        }
+    }
+
+// function to edit bill
+const editBilling = async (req, res) => {
+    try {
+        const { id } = req.params
+
+        const billing = await billingModel.findById(id).populate('customer').populate('products.product')
+
+        if (!billing) {
+            return res.status(404).json({ message: 'Billing record not found' })
+        }
+
+        res.status(200).json({ billing })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: error.message })
+    }
+}
+
+// Function to delete billing record
+const deleteBill = async (req, res) => {
+
+    try {
+        const { id } = req.params 
+
+        const deletedBilling = await billingModel.findByIdAndDelete(id)
+
+        if (!deletedBilling) return res.status(404).json({ message: 'Billing record not found' })
+
+        res.status(200).json({ message: 'Record deleted successfully' })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: error.message })
+    }
+}
+
 module.exports = {
-    createBill
+    createBill,
+    editBilling,
+    updateBill,
+    deleteBill
+
 }
--- Server/routes/billing.routes.js
+++ Server/routes/billing.routes.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const isEmpty = require('../middlewares/isEmpty')
-const { createBill } = require('../controllers/billing.controller')
+const { createBill, deleteBill, editBilling, updateBill } = require('../controllers/billing.controller')
 
 const route = express.Router()
 
 route.post('/create-bill', isEmpty, createBill)
 
-// route.get('/view')
+route.get('/edit/:id', editBilling)
+
+route.post('/update/:id', isEmpty, updateBill)
+
+route.get('/delete/:id', deleteBill)
 
 module.exports = route
